12cb509372a34e4b4a52f346d76383cddc072776
amateurguts <elibaevdavid812@gmail.com>
2026-02-24 04:44:59 +0400

    Update main.py

--- main.py
+++ main.py
@@ -5,17 +5,12 @@ app = Flask(__name__, template_folder='.', static_folder='.')
 
 @app.route('/')
 def index():
-    
     return render_template('index.html')
 
 @app.route('/about')
 def about():
-    
     return render_template('about.html')
 
 @app.route('/<path:path>')
 def send_static(path):
     return send_from_directory('.', path)
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
